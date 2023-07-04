7a788ef15f7312125dd63ea34f2e9ba57f867dd6
Sergey <inboxshow1@gmail.com>
2023-07-04 22:54:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-const futureDate = '1 Jan 2024'
+
 let days = document.querySelector('.timer-day__count')
 let hours = document.querySelector('.timer-hours__count')
 let minutes = document.querySelector('.timer-minutes__count')
 let seconds = document.querySelector('.timer-seconds__count')
 
-const countDown =()=>{
+const countDown = () => {
     const now = new Date()
-    const date = new Date(futureDate)
-    let diff = date - now 
-    console.log(diff);
+    const futureYear = now.getFullYear() + 1 // Год следующего года
+    const futureDate = `1 Jan ${futureYear}` // Обновленная дата с будущим годом
 
-    days.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60 * 60 * 24))}`) 
-    hours.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60 * 60))%24}`) 
-    minutes.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60 ))%60}`) 
-    seconds.innerHTML = timeCorrection(`${Math.round(diff / (1000)%60)}`) 
+    const date = new Date(futureDate)
+    let diff = date - now
+
+    days.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60 * 60 * 24))}`)
+    hours.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60 * 60)) % 24}`)
+    minutes.innerHTML = timeCorrection(`${Math.round(diff / (1000 * 60)) % 60}`)
+    seconds.innerHTML = timeCorrection(`${Math.round(diff / 1000) % 60}`)
 }
 
-function timeCorrection(time){
+function timeCorrection(time) {
     return time < 10 ? `0${time}` : time
 }
+
 countDown()
-setInterval(countDown, 1000)
+setInterval(countDown, 1000))
 
 
 
